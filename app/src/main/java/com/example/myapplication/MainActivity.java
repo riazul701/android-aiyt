0f76b27901e79600f7851c503caeb222b73ff516
Work2 <work2@pc>
2020-05-04 04:57:10 +0600

    Code added from 014_Textview_in_android_example_part4.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView nameTextView;
+    private TextView ageTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameTextView = (TextView) findViewById(R.id.newTextViewId);
+        ageTextView = (TextView) findViewById(R.id.ageTextViewId);
+
+        nameTextView.setText("Name Eleven");
+        ageTextView.setText("25 years old");
     }
 }
