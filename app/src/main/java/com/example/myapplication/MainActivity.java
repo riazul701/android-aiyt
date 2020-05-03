f4f43c60cdf1fd256b03121ed0c1937d94cf8771
Work2 <work2@pc>
2020-05-04 05:52:18 +0600

    Code added from 018_Button_with_Listener_part1.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
+    int count = 0;
+    private Button loginButton;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.loginButtonId);
+        textView = (TextView) findViewById(R.id.textViewId);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                textView.setText("Login button is clicked "+count+" times");
+            }
+        });
     }
 }
+
