c99f259df4add340599e9f494a83ad9faa41d1ba
Work2 <work2@pc>
2020-05-05 14:59:49 +0600

    Code added from 029_Displaying_image_using_imageview_part2.
    Tutorial 029: Displaying image in android studio using
    imageview (part-2).

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imageView1, imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView1 = (ImageView) findViewById(R.id.imageView1Id);
+        imageView2 = (ImageView) findViewById(R.id.imageView2Id);
+
+        imageView1.setOnClickListener(this);
+        imageView2.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.imageView1Id) {
+            Toast.makeText(MainActivity.this, "Clicked on First image", Toast.LENGTH_SHORT).show();
+        } else if (v.getId() == R.id.imageView2Id) {
+            Toast.makeText(MainActivity.this, "Clicked on Second image", Toast.LENGTH_SHORT).show();
+        }
     }
 }
