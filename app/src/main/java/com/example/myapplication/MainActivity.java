c9aa3fb4c9a1d5c92f08e79c2a55e76590eec7b1
Work2 <work2@pc>
2020-05-05 15:16:31 +0600

    Code added from 031_Custom_toast_part2.
    Tutorial 031: Custom toast in android example (part-2).

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,13 +3,36 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.loginButtonId);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = getLayoutInflater();
+                View customView = inflater.inflate(R.layout.customtoast_layout,(ViewGroup) findViewById(R.id.customtoast_id));
+
+                Toast toast = new Toast(MainActivity.this);
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.setView(customView);
+                toast.show();
+            }
+        });
     }
 }
+
 
