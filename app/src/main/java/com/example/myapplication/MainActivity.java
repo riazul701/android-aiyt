f02af35c82f26cf465902110613c8be48baf2039
Work2 <work2@pc>
2020-05-05 15:36:38 +0600

    Code added from 035_Ratingbar.
    Tutorial 035: Ratingbar in android studio example.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBarId);
+        textView = (TextView) findViewById(R.id.textViewId);
+        textView.setText("Value : "+ratingBar.getProgress());
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                textView.setText("Value : "+rating);
+            }
+        });
     }
 }
