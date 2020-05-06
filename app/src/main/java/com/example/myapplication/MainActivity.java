f39a6e9ad919128297953fea5981b3bb6c45ffac
Work2 <work2@pc>
2020-05-06 09:53:28 +0600

    Code added from 041_Datepicker.
    Tutorial 041: Datepicker in android studio example.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private DatePicker datePicker;
+    private Button selectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textViewId);
+        datePicker = (DatePicker) findViewById(R.id.datePickerId);
+        selectButton = (Button) findViewById(R.id.buttonId);
+
+        textView.setText(currentDate());
+
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(currentDate());
+            }
+        });
+    }
+
+    String currentDate() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("Current Date : ");
+        stringBuilder.append(datePicker.getDayOfMonth() + "/");
+        stringBuilder.append((datePicker.getMonth() + 1) + "/");
+        stringBuilder.append(datePicker.getYear());
+
+        return stringBuilder.toString();
     }
 }
