1c9dfe58a626d4570a529a34fe31d4c43f61df60
Work2 <work2@pc>
2020-05-06 10:16:01 +0600

    Code added from 045_Android_clock.
    Tutorial 045: Android clock tutorial.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AnalogClock;
+import android.widget.TextClock;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextClock textClock;
+    private AnalogClock analogClock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textClock = (TextClock) findViewById(R.id.textClockId);
+        analogClock = (AnalogClock) findViewById(R.id.analogClockId);
+
+        textClock.setOnClickListener(this);
+        analogClock.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.textClockId) {
+            Toast.makeText(MainActivity.this, "Text clock", Toast.LENGTH_SHORT).show();
+        }
+        if (v.getId() == R.id.analogClockId) {
+            Toast.makeText(MainActivity.this, "Analog clock", Toast.LENGTH_SHORT).show();
+        }
     }
 }
