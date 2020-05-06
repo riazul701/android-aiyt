e925b42ab3c35d1c97ee243533bbb8913606a208
Work2 <work2@pc>
2020-05-06 10:04:43 +0600

    Code added from 043_Timepicker.
    Tutorial 043: Timepicker android tutorial.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TimePicker timePicker;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timePicker = (TimePicker) findViewById(R.id.timePickerId);
+        timePicker.setIs24HourView(true);
+        textView = (TextView) findViewById(R.id.textViewId);
+        button = (Button) findViewById(R.id.buttonId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String time = timePicker.getCurrentHour() + ":" + timePicker.getCurrentMinute();
+                textView.setText(time);
+            }
+        });
     }
 }
