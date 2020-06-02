473380614053e42f812a7255efb850b34e88632c
Riazul Hasan <riazul701@gmail.com>
2020-06-02 14:54:37 +0600

    Code added from 049_ZoomControls.
    Tutorial_049: ZoomControls in android.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.myapplication;
 
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ZoomControls;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private ZoomControls zoomControls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageViewId);
+        zoomControls = (ZoomControls) findViewById(R.id.zoomControlId);
+
+        zoomControls.setOnZoomInClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Zoom in", Toast.LENGTH_SHORT).show();
+                float x = imageView.getScaleX();
+                float y = imageView.getScaleY();
+
+                imageView.setScaleX((float) x + 1);
+                imageView.setScaleY((float) y + 1);
+            }
+        });
+
+        zoomControls.setOnZoomOutClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Zoom out", Toast.LENGTH_SHORT).show();
+                float x = imageView.getScaleX();
+                float y = imageView.getScaleY();
+
+                if (x > 1 && y > 1) {
+                    imageView.setScaleX((float) x - 1);
+                    imageView.setScaleY((float) y - 1);
+                }
+            }
+        });
     }
 }
