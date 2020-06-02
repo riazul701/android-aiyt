527cc482ee69ff46f75d7fb7c93b47dad691f7c1
Riazul Hasan <riazul701@gmail.com>
2020-06-02 21:32:50 +0600

    Code from 073_FullScreen_Activity.
    Tutorial_073: FullScreen Activity.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    int progress;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Remove the title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Remove the notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBarId);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+            }
+        });
+        thread.start();
+    }
+
+    public void doWork() {
+        for (progress=20; progress<=100; progress=progress+20) {
+            try {
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
