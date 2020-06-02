96285e8d1ec223b199a7e1170adad60d0e33f427
Riazul Hasan <riazul701@gmail.com>
2020-06-03 01:59:01 +0600

    Code from 091_Expandable_Listadapter_part2.
    Tutorial_091: Expandable Listadapter android example (part-2).

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.myapplication;
 
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ExpandableListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ExpandableListView expandableListView;
+    private CustomAdapter customAdapter;
+    List<String> listDataHeader;
+    HashMap<String,List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prepareListData();
+
+        expandableListView = (ExpandableListView) findViewById(R.id.expandableListViewId);
+        customAdapter = new CustomAdapter(this,listDataHeader,listDataChild);
+        expandableListView.setAdapter(customAdapter);
+
+    }
+
+    public void prepareListData() {
+        String[] headerString = getResources().getStringArray(R.array.abbreviations_list_header);
+        String[] childString = getResources().getStringArray(R.array.abbreviations_list_child);
+
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
+
+        for(int i=0; i<headerString.length; i++) {
+            // adding header data
+            listDataHeader.add(headerString[i]);
+
+            List<String> child = new ArrayList<>();
+            child.add(childString[i]);
+
+            listDataChild.put(listDataHeader.get(i),child);
+        }
     }
 }
