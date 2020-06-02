0067a8b6a4b083a86bd90e40a9fd53e5aa5167c2
Riazul Hasan <riazul701@gmail.com>
2020-06-02 19:58:12 +0600

    Code from 063_Listview.
    Tutorial_063: Listview in android studio example.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.myapplication;
 
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listViewId);
+        String[] countryNames = getResources().getStringArray(R.array.country_names);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.sample_view,R.id.textViewId,countryNames);
+        listView.setAdapter(adapter);
     }
 }
