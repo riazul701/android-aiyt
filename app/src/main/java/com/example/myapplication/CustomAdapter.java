1317428db91bc14ac54be2ee861abf3c3f588a57
Riazul Hasan <riazul701@gmail.com>
2020-06-02 21:17:30 +0600

    Code from 071_Spinner_with_OnItemSelected_Listener_part2.
    Tutorial_071: Spinner with OnItemSelected Listener (part-2).

---
+++ app/src/main/java/com/example/myapplication/CustomAdapter.java
@@ -0,0 +1,64 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by dell on 2/12/18.
+ */
+
+public class CustomAdapter extends BaseAdapter {
+
+    private String[] countryNames;
+    private String[] population;
+    int[] flags;
+    Context context;
+    private LayoutInflater layoutInflater;
+
+    public CustomAdapter(Context context,int[] flags,String[] countryNames,String[] population) {
+
+        this.countryNames = countryNames;
+        this.population = population;
+        this.flags = flags;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return countryNames.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(view == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(R.layout.sample_view,null,false);
+        }
+
+        ImageView imageView = view.findViewById(R.id.imageViewId);
+        imageView.setImageResource(flags[i]);
+
+        TextView country = view.findViewById(R.id.countrynameTextViewId);
+        country.setText(countryNames[i]);
+
+        TextView populationNumber = view.findViewById(R.id.populationTextViewId);
+        populationNumber.setText(population[i]);
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.myapplication;
 
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private String[] countryNames;
+    private String[] population;
+    private int[] flags = {R.drawable.afghanistan,R.drawable.bangladesh,R.drawable.india,R.drawable.bhutan,R.drawable.nepal,R.drawable.pakistan,R.drawable.japan,R.drawable.china,R.drawable.sri_lanka};
+    private boolean isFirstSelection = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countryNames = getResources().getStringArray(R.array.country_name);
+        population = getResources().getStringArray(R.array.population);
+        spinner = (Spinner) findViewById(R.id.spinnerId);
+
+        CustomAdapter customAdapter = new CustomAdapter(this,flags,countryNames,population);
+        spinner.setAdapter(customAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(isFirstSelection == true) {
+                    isFirstSelection = false;
+                } else {
+                    Toast.makeText(getApplicationContext(),countryNames[i]+" is selected",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
