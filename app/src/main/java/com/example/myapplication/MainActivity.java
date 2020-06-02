363e9f4389c47c2dc8175cb0ce7b941fee0c02fe
Riazul Hasan <riazul701@gmail.com>
2020-06-02 22:11:38 +0600

    Code from 075_ListView_Filtering_and_SearchView.
    Tutorial_075: ListView Filtering and SearchView.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.myapplication;
 
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    String[] countryNames;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listViewId);
+        searchView = (SearchView) findViewById(R.id.searchViewId);
+        countryNames = getResources().getStringArray(R.array.country_name);
+
+        adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.sample_view,R.id.textViewId,countryNames);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener((AdapterView.OnItemClickListener) this);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String value = adapter.getItem(position);
+        Toast.makeText(MainActivity.this,value,Toast.LENGTH_SHORT).show();
     }
 }
