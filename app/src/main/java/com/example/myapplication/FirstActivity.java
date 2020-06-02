8712b70286af781073b1d5054af3a6f683ed74f6
Riazul Hasan <riazul701@gmail.com>
2020-06-02 15:34:42 +0600

    Code added from 054_Intent.
    Tutorial_054: Intent in android studio tutorial.

---
+++ app/src/main/java/com/example/myapplication/FirstActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity {
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        button = (Button) findViewById(R.id.buttonId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
