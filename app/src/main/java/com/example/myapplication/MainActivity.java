4aaa5b74db6a8cd439af7ac5e6c033b43d466b04
Riazul Hasan <riazul701@gmail.com>
2020-06-02 18:57:09 +0600

    Code from 058_Countryprofile_project_part3.
    Tutorial_058: Countryprofile Project (part-3).

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+//import android.support.v7.app.AlertDialog;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -44,5 +47,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             intent.putExtra("name","pakistan");
             startActivity(intent);
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder alertDialogBuilder;
+        alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setIcon(R.drawable.question);
+        alertDialogBuilder.setTitle(R.string.title_text);
+        alertDialogBuilder.setMessage(R.string.message_text);
+        alertDialogBuilder.setCancelable(false);
+
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
