8fbffce84d1e5c7bcc46683f61e596ef4f3c5862
Riazul Hasan <riazul701@gmail.com>
2020-06-02 22:30:35 +0600

    Code from 076_Option_menu.
    Tutorial_076: Option menu in android studio example with code.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_layout,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.settingId) {
+            Toast.makeText(MainActivity.this,"Setting is selelected",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if(item.getItemId() == R.id.shareId) {
+            Toast.makeText(MainActivity.this,"Share is selelected",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if(item.getItemId() == R.id.feedbackId) {
+            Toast.makeText(MainActivity.this,"Feedback is selelected",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if(item.getItemId() == R.id.aboutusId) {
+            Toast.makeText(MainActivity.this,"About us is selelected",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
