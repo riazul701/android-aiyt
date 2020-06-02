a0d69f9f91eaf35eec2b1731bc4e16ecfed4cb95
Riazul Hasan <riazul701@gmail.com>
2020-06-03 00:04:37 +0600

    Code from 079_Mediaplayer.
    Tutorial_079: Mediaplayer in android studio.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.myapplication;
 
+import android.media.MediaPlayer;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private ImageButton playButton, pauseButton;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playButton = (ImageButton) findViewById(R.id.playButtonId);
+        pauseButton = (ImageButton) findViewById(R.id.pauseButtonId);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.horse);
+        playButton.setOnClickListener(this);
+        pauseButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.playButtonId) {
+            if (mediaPlayer != null) {
+                mediaPlayer.start();
+                int duration = mediaPlayer.getDuration()/1000;
+                Toast.makeText(MainActivity.this, "Song Played "+duration, Toast.LENGTH_SHORT).show();
+            }
+        }
+        if (v.getId() == R.id.pauseButtonId) {
+            mediaPlayer.pause();
+            Toast.makeText(MainActivity.this, "Song Paused", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(mediaPlayer!=null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer=null;
+        }
+        super.onDestroy();
     }
 }
+
