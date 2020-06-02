d4f6ee8efbe6564da88f1266c642ae9aa6c081ab
Riazul Hasan <riazul701@gmail.com>
2020-06-02 19:16:10 +0600

    Code from 059_onActivityForResult.
    Tutorial_059: onActivityForResult in android.

---
+++ app/src/main/java/com/example/myapplication/FirstActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button1Id);
+        textView = (TextView) findViewById(R.id.textViewId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1) {
+            String value = data.getStringExtra("key");
+            textView.setText(value);
+        }
+    }
+}
