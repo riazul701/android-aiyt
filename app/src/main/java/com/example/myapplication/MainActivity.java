af948defa9e2e819de6050067eec6f8b28a6afd5
Riazul Hasan <riazul701@gmail.com>
2020-06-02 22:02:16 +0600

    Code from 074_SplashScreen.
    Tutorial_074: How To Create SplashScreen.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
