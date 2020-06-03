370412674d70dc7be8b9067a910fbfbba0367374
Riazul Hasan <riazul701@gmail.com>
2020-06-03 16:51:09 +0600

    Code from 099_Relative_Layout.
    Tutorial_099: Relative Layout android example.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
