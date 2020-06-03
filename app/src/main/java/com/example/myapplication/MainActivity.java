59f91d4823b6c07eb89f1d78fcb33a6fd05cbd67
Riazul Hasan <riazul701@gmail.com>
2020-06-04 01:15:29 +0600

    Code from 120_Storing_data_using_file_part2.
    Tutorial_120: Storing data using file (part-2).

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,81 @@
 package com.example.myapplication;
 
+import android.content.Context;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editTextId);
+        saveButton = (Button) findViewById(R.id.saveButtonId);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String text = editText.getText().toString();
+                if (text != null) {
+                    writeToFile(text);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter some data", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+        readFromFile();
+    }
+
+    public void writeToFile(String text) {
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("diary.txt", Context.MODE_PRIVATE);
+            fileOutputStream.write(text.getBytes());
+            fileOutputStream.close();
+            Toast.makeText(getApplicationContext(), "data is saved", Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readFromFile() {
+        try {
+            FileInputStream fileInputStream = openFileInput("diary.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line;
+            StringBuffer stringBuffer = new StringBuffer();
+
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line+"\n");
+            }
+            editText.setText(stringBuffer.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
