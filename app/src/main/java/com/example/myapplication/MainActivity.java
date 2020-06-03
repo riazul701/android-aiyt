2decc431969fa4381d28db5346d3cd344ab5c14e
Riazul Hasan <riazul701@gmail.com>
2020-06-03 11:30:42 +0600

    Code from 092_Expandable_Listview_With_Listener_part1.
    Tutorial_092: Expandable Listview With Listener (part-1).

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.myapplication;
 
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ExpandableListView expandableListView;
+    private CustomAdapter customAdapter;
+    List<String> listDataHeader;
+    HashMap<String,List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prepareListData();
+
+        expandableListView = (ExpandableListView) findViewById(R.id.expandableListViewId);
+        customAdapter = new CustomAdapter(this,listDataHeader,listDataChild);
+        expandableListView.setAdapter(customAdapter);
+
+        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView expandableListView, View view, int i, long l) {
+                String groupName = listDataHeader.get(i);
+                Toast.makeText(getApplicationContext(),"group name : "+groupName,Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int i) {
+                String groupName = listDataHeader.get(i);
+                Toast.makeText(getApplicationContext(),groupName+" is collapsed",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
+                String childString = listDataChild.get(listDataHeader.get(i)).get(i1);
+                Toast.makeText(getApplicationContext(),childString,Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+    }
+
+    public void prepareListData() {
+        String[] headerString = getResources().getStringArray(R.array.abbreviations_list_header);
+        String[] childString = getResources().getStringArray(R.array.abbreviations_list_child);
+
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
+
+        for(int i=0; i<headerString.length; i++) {
+            // adding header data
+            listDataHeader.add(headerString[i]);
+
+            List<String> child = new ArrayList<>();
+            child.add(childString[i]);
+
+            listDataChild.put(listDataHeader.get(i),child);
+        }
     }
 }
