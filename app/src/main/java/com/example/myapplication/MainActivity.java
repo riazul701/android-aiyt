618c2d1ab79d7f6fd4931f8dc025da41b6309f91
Riazul Hasan <riazul701@gmail.com>
2020-06-03 17:17:29 +0600

    Code from 101_Frame_Layout.
    Tutorial_101: Frame Layout android example.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.myapplication;
 
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private ImageView strawberryImageView, orangeImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        strawberryImageView = (ImageView) findViewById(R.id.strawberryImageViewId);
+        orangeImageView = (ImageView) findViewById(R.id.orangeImageViewId);
+
+        strawberryImageView.setOnClickListener(this);
+        orangeImageView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.strawberryImageViewId) {
+            strawberryImageView.setVisibility(View.GONE);
+            orangeImageView.setVisibility(View.VISIBLE);
+        } else if (view.getId() == R.id.orangeImageViewId) {
+            orangeImageView.setVisibility(View.GONE);
+            strawberryImageView.setVisibility(View.VISIBLE);
+        }
     }
 }
