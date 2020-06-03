3d8e2e5327fc200ce76bc4003639447b6e9b21b1
Riazul Hasan <riazul701@gmail.com>
2020-06-03 20:42:39 +0600

    Code from 106_Login_Application_part2.
    Tutorial_106: How to design and create a Login Application (part-2).

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText, passwordEditText;
+    private Button loginButton;
+    private TextView textView;
+    private int counter = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameEditText = (EditText) findViewById(R.id.usernameId);
+        passwordEditText = (EditText) findViewById(R.id.passwordId);
+        loginButton = (Button) findViewById(R.id.loginButtonId);
+        textView = (TextView) findViewById(R.id.textViewId);
+        textView.setText("Number of attempts remaining : 3");
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (username.equals("admin") && password.equals("1234")) {
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+                } else {
+                    counter--;
+                    textView.setText("Number of attempts remaining : " + counter);
+                    if (counter == 0) {
+                        loginButton.setEnabled(false);
+                    }
+                }
+            }
+        });
     }
 }
