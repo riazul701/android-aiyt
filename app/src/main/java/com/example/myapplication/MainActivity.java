7bd0b42fe6a3908de5c9919cb0e77dddf5c966dc
Riazul Hasan <riazul701@gmail.com>
2020-06-03 16:08:08 +0600

    Code from 096_Fragment_part2.
    Tutorial_096: Fragment android example (Part-2).

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.myapplication;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] name = {"First", "Second", "Third", "Fourth", "Fifth"};
+
+        listView = (ListView) findViewById(R.id.listViewId);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, name);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if (i == 0) {
+            Fragment fragment = new FirstFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentId,fragment);
+            fragmentTransaction.commit();
+        } else if (i == 1) {
+            Fragment fragment = new SecondFragment();
+            getFragmentManager().beginTransaction().replace(R.id.fragmentId,fragment).commit();
+        }
+
     }
 }
