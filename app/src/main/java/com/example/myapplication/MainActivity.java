8ddd7670565a1ace1247ad9cee708a402f8e7275
Riazul Hasan <riazul701@gmail.com>
2020-06-07 18:13:13 +0600

    Migrated from AndroidX to AppCompat.
    Downgraded from AndroidX to AppCompat to run old code properly.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.myapplication;
 
-//import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
